d53c44e8d11289229d8072849f01834f228409ce
JucanaYu <705027616@qq.com>
2023-10-26 11:59:24 +0800

    add some comments

--- 6-81.c
+++ 6-81.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 int main(){
+    struct timespec start_time, end_time;
+    unsigned long long elapsed_time;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
     int a =1,b=5;
     if((a+b)>3 && (a+b)<10){
         a=a+b;
     }
     printf("%d",a);
+
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
+    elapsed_time = (end_time.tv_sec - start_time.tv_sec) * 1000000000ULL + (end_time.tv_nsec - start_time.tv_nsec);
+    printf("Execution Time: %llu ns\n", elapsed_time);
 }
--- 6-82.c
+++ 6-82.c
@@ -1,11 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<time.h>
 
 int main(){
+    struct timespec start_time, end_time;
+    unsigned long long elapsed_time;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
     int a =1,b=5;
     int tmp=a+b;
     if(tmp>3 && tmp<10){
         a=tmp;
     }
     printf("%d",a);
+
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
+    elapsed_time = (end_time.tv_sec - start_time.tv_sec) * 1000000000ULL + (end_time.tv_nsec - start_time.tv_nsec);
+    printf("Execution Time: %llu ns\n", elapsed_time);
 }
